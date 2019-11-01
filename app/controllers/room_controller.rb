0187a35b50f1bc11d54154a844e3b3a5f458cbef
George Zhang <georgezhang@ucla.edu>
2019-11-01 14:12:31 -0700

    readd pg and undo stuff

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -16,7 +16,7 @@ class RoomController < ApplicationController
       redirect_to '/'
     else
       @room = Room.create(name: name)
-      redirect_to "/room/"
+      redirect_to "/room/#{name}"
     end
   end
 
