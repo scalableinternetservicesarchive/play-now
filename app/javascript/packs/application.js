4a9bb609e363bb60dcf5ed997d5863f3ac48a5e1
George Zhang <georgezhang@ucla.edu>
2019-11-22 15:25:28 -0800

    add second set of form controllers to view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,7 @@ require("channels");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// TODO: need to poll every second
+// SetInterval here with a fetch request
+// also need to have client updating logic here — callPlayer
